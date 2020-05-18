6162fa10267c131e956063d334047c8d4f6cc904
mateuszkowalke <mateusz.kowalke@gmail.com>
2020-05-18 15:57:09 +0200

    Change models to store multiple images per falcon

--- breeding/admin.py
+++ breeding/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Falcon, Pair, Aviary
+from .models import Falcon, Pair, Aviary, Photo
 
 admin.site.register(Falcon)
 admin.site.register(Pair)
 admin.site.register(Aviary)
+admin.site.register(Photo)
--- breeding/models.py
+++ breeding/models.py
@@ -17,9 +17,9 @@ class Falcon(models.Model):
     sex = models.CharField(max_length=1, choices=SEX, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     CITES_num = models.CharField(max_length=30, null=True, blank=True)
-    CITES_img = models.ImageField(null=True, blank=True)
-    registration_img = models.ImageField(null=True, blank=True)
-    RDOS_permission_img = models.ImageField(null=True, blank=True)
+    CITES_img = models.FileField(null=True, blank=True, upload_to='falcon_docs/')
+    registration_img = models.FileField(null=True, blank=True, upload_to='falcon_docs/')
+    RDOS_permission_img = models.FileField(null=True, blank=True, upload_to='falcon_docs/')
     in_aviary = models.ForeignKey('Aviary', on_delete=models.CASCADE, null=True, blank=True)
     in_pair = models.ForeignKey('Pair', on_delete=models.CASCADE, null=True, blank=True)
     father = models.ForeignKey('self', on_delete=models.CASCADE, related_name='_father', null=True, blank=True)
@@ -28,14 +28,14 @@ class Falcon(models.Model):
     width_young = models.PositiveSmallIntegerField(null=True, blank=True)
     length_young = models.PositiveSmallIntegerField(null=True, blank=True)
     weight_young = models.PositiveSmallIntegerField(null=True, blank=True)
-    photos_young = models.ImageField(null=True, blank=True)
+    photos_young = models.ForeignKey('Photo', on_delete=models.CASCADE, related_name='_photos_young', null=True, blank=True)
     width_old = models.PositiveSmallIntegerField(null=True, blank=True)
     length_old = models.PositiveSmallIntegerField(null=True, blank=True)
     weight_old = models.PositiveSmallIntegerField(null=True, blank=True)
-    photos_old = models.ImageField(null=True, blank=True)
+    photos_old = models.ForeignKey('Photo', on_delete=models.CASCADE, related_name='_photos_old', null=True, blank=True)
 
     def get_fields_for_template(self):
-        return [(field.verbose_name, field.value_to_string(self)) for field in Falcon._meta.fields]
+        return [(field.name, field.verbose_name, field.value_to_string(self)) for field in Falcon._meta.fields]
 
     def __str__(self):
         return 'Falcon name: ' + str(self.name) + ', ring: ' + str(self.ring)
@@ -48,7 +48,6 @@ class Pair(models.Model):
     offspring = models.ManyToManyField(Falcon, blank=True)
 
     def __str__(self):
-        print(self.keys())
         return 'Pair:\nmale name: ' + str(self.male.name) + ', ring: ' + str(self.male.ring) + ';\n' + 'female name: ' + str(self.female.name) + ', ring: ' + str(self.female.ring)
 
 class Aviary(models.Model):
@@ -57,6 +56,15 @@ class Aviary(models.Model):
     occupied = models.BooleanField()
     pair = models.ForeignKey(Pair, on_delete=models.CASCADE, null=True, blank=True)
     falcons = models.ManyToManyField(Falcon, blank=True)
+    last_cleaned = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return 'Aviary id: ' + str(self.id) + '\noccupied: ' + str(self.occupied)
+
+class Photo(models.Model):
+    """ Model to enable saving multiple photos per falcon """
+
+    img = models.ImageField(null=True, blank=True, upload_to='falcon_imgs/')
+
+    def __str__(self):
+        return 'Falcon Photo'
