f37fcfbbc49c8b50e56ab4dde2f16c540b5dead6
mateuszkowalke <mateusz.kowalke@gmail.com>
2020-06-28 10:15:16 +0200

    Security changes

--- falcon_manager/settings.py
+++ falcon_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i$e1$svhkbshfu(yz@iz88i+r&%b(v&u=so5%7jd52nak(&5r+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
