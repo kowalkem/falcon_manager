45e286965c52d495a48d541a5418d2749ef4f6eb
mateuszkowalke <mateusz.kowalke@gmail.com>
2021-05-03 12:05:18 +0200

    fix

--- falcon_manager/settings.py
+++ falcon_manager/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS: List[str] = ['172.104.248.81']
 
 
 # Application definition
